7961d10e7066699c8185c4a7ec01b7a32b2e4011
Hygor Silva <77420686+hygor-silva@users.noreply.github.com>
2021-08-08 19:34:03 -0300

    PostMapping
    Ajustes e inclusão do GetMappingById e PostMapping

--- src/main/java/com/feedback/resources/FeedbackResource.java
+++ src/main/java/com/feedback/resources/FeedbackResource.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +16,13 @@ import com.feedback.repositories.FeedbackRepository;
 
 
 @RestController
-@RequestMapping(value = "/feedback" )
+@RequestMapping
 public class FeedbackResource {
 
 	@Autowired
 	private FeedbackRepository repository;
 	
-	//getmapping, postmapping, deletemapping e patchmapping
-	
+	//Lista todos os feedbacks cadastrados.
 	@GetMapping
 	public ResponseEntity<List<Feedback>>listarFeedback(){
 		List<Feedback> list = repository.findAll();
@@ -28,6 +30,20 @@ public class FeedbackResource {
 		return ResponseEntity.ok().body(list);
 		
 	}
+	
+	//Lista feedback pesquisado pelo Id
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Feedback>findById(@PathVariable Long id){
+		Feedback feed = repository.findById(id).get();
+		return ResponseEntity.ok().body(feed);
+	}
+	
+	//Cadastra feedbacks
+	@PostMapping
+	public void cadastroFeedback(@RequestBody Feedback feedback) {
+		repository.save(feedback);
+	}
+	
 	
 	
 }
